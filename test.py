c7aa93a5fa774e186bf18b12f5dda17fe6133d29
Jay Mirecki <jarett.mirecki@gmail.com>
2019-09-18 13:11:08 -0400

    Added photo

---
+++ test.py
@@ -0,0 +1,8 @@
+def hello():
+    name = raw_input("What's your name?\n")
+    if name == name.upper():
+        print "You're shouting!"
+    else:
+        print "I can't hear you..."
+hello()
+hello()
