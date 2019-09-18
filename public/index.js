06b9a9b14df40acd3cb94777afb28b2f7e1d285d
Jay Mirecki <jarett.mirecki@gmail.com>
2019-09-18 14:40:11 -0400

    Mobile hotfix

--- public/index.js
+++ public/index.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
+    loadStyle();
     jaysays();
 });
 
 var sayings = ["You're awesome!"];
 
 updateSayings();
+
+function loadStyle() {
+    var stylesheet = document.createElement('link');
+    stylesheet.rel = 'stylesheet';
+    stylesheet.type = 'text/css';
+    var mobile =  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    console.log(mobile);
+    if (mobile)
+        // console.log("Mobile");
+        stylesheet.href = '/mobile.css';
+    else
+        stylesheet.href = '/desktop.css';
+    document.getElementsByTagName('head')[0].appendChild(stylesheet);
+}
 
 function updateSayings() {
     var request = new XMLHttpRequest();
